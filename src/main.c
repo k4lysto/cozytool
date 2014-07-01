a4395afcce2b0d372377e357c2f21296bd43eed8
Cozypet <yanhan860711@gmail.com>
2014-07-01 11:08:31 +0200

    main.c

--- src/main.c
+++ src/main.c
@@ -1,22 +1,33 @@
+#include "dbg.h"
 #include <stdio.h>
-#include <errno.h>
 #include <string.h>
 
-#ifdef NDEBUG
-#define debug(M,...)
-#else 
-#define debug(M,...) fprintf(stderr, "DEBUG %s %d: " M "\n", __FILE__, __LINE__,##__VA_ARGS__)
-#endif 
+void test_debug(){
+	debug("Cozypet tests the debug for the %d time.\n ", 2);
+
+}
+
+void test_info_err_warn(){
+
+	DEBUG_INFO("Cozypet is testing the debug for the %d time.\n", 2);
+	DEBUG_ERR("Cozypet tests the debug for the %d time.\n", 1);
+	DEBUG_WARN("Be careful! Cozypet tests the debug for the %d time.\n", 2);
+
+}
 
 int main(int argc, char* argv[])
 {	
+	test_debug();
 	
+	test_info_err_warn();
+
 	if(argc < 2){
-		printf("You need to put an argument.");	
+		printf("You need to put an argument.\n");	
 	}else{
 		if( *argv[2] == 'h')
 		{
 			printf("Help message");
 		}
 	}
+	return 0;
 }
