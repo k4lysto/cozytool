098196eab129ab8892a231627c50aeaf749801bf
k4lysto <p3g4se@gmail.com>
2014-07-30 13:56:08 +0200

    P0 corrections

--- src/dbg.h
+++ src/dbg.h
@@ -5,26 +5,19 @@
 #include <errno.h>
 #include <string.h>
 
-#ifdef NDEBUG
-#define debug(M, ...)
+#ifdef SHOW_DEBUG
+#define debug(M, ...) fprintf(stderr, M, ##__VA_ARGS__)
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
-
-#define DEBUG_ERR(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
-
-#define DEBUG_WARN(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
-
-#define DEBUG_INFO(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-
+#define DEBUG_ERR(M, ...) debug("[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define DEBUG_WARN(M, ...) debug("[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define DEBUG_INFO(M, ...) debug("[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
-
 #define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
-
 #define check_mem(A) check((A), "Out of memory.")
-
 #define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; }
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,58 +1,91 @@
-#include "dbg.h"
+/**
+ * @file main.c
+ *
+ * @brief cozytool main function
+ *
+ * Copyright (C) 2014-2015 CozyCorp
+ */
+
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include "dbg.h"
 
-void test_debug(){
+#define PROGRAM_NAME "cozytool"
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+
+const char USAGE[] = "\
+ Usage : %s  [OPTIONS]\n\
+ \n\
+ An awsome tool doing a lot of marvelous things\n\
+ \n\
+ [OPTIONS]\n\
+   -h                        Usage help\n\
+   -v                        Verbose trace mode\n\
+   -f delete:non-ascii       delete non-ascii/printables chars from STDIN input\n\
+ \n\
+ (Copyright CozyCorp 2014 - Release v%d.%02d - %s)\n\
+ ";
+
+void test_debug()
+{
 	debug("Cozypet tests the debug for the %d time.\n ", 2);
-
 }
 
-void test_info_err_warn(){
-
+void test_info_err_warn()
+{
 	DEBUG_INFO("Cozypet is testing the debug for the %d time.\n", 2);
 	DEBUG_ERR("Cozypet tests the debug for the %d time.\n", 1);
 	DEBUG_WARN("Be careful! Cozypet tests the debug for the %d time.\n", 2);
-
 }
+
+void usage(void)
+{
+    fprintf(stderr, USAGE, PROGRAM_NAME, VERSION_MAJOR, VERSION_MINOR, __DATE__);
+    exit(EXIT_FAILURE);
+}
+
 
 int main(int argc, char* argv[])
 {	
-//	char *hvalue = NULL;
-	char *fvalue = NULL;
-	int index;
 	int option;
-	opterr = 0;
-	test_debug();
-	test_info_err_warn();
-	while ((option = getopt (argc, argv, "hf:")) != -1)
+	int flag_filter_non_ascii = 0;
+	int n;
+	char ch;
+	
+//	test_debug();
+//	test_info_err_warn();
+	while ((option = getopt(argc, argv, "hf:")) != -1) {
 		switch (option)
-			{
-			case 'h':
-				//hvalue = optarg;
-				printf("This tool is a text tool.\n");
-			break;
-			case 'f':
-				fvalue = optarg;	
-				if(isalpha(atoi(fvalue))){
-					fprintf(stderr,"This is not a alpha num value: %s.\n", fvalue);
-				}else fprintf(stderr, "This is a alpha num value: %s.\n", fvalue);
-			break;
-			case '?':
-				if (optopt == 'f')
-					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-				else if (isprint (optopt))
-					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-				else
-					fprintf (stderr,"Unknown option character `\\x%x'.\n",optopt);
-				return 1;
-			default:
-			abort ();
+		{
+		case 'f':
+			if (strstr(optarg, "delete:non-ascii")) {
+				flag_filter_non_ascii = 1;
+				DEBUG_INFO("filter activated: delete:non-ascii");
+			} else {
+				usage();
 			}
-	printf (" fvalue = %s\n", fvalue);
-	for (index = optind; index < argc; index++)printf ("Non-option argument %s\n", argv[index]);
+			break;
+		case 'h':
+		default:
+			usage();
+		}
+	}
+
+	
+	while (1) {
+		n = read(STDIN_FILENO, (void *)&ch, 1);
+		if (n <= 0) {
+			return 0;
+		}
+		if (flag_filter_non_ascii && (! isprint(ch))) {
+			continue; // do nothing
+		}
+		write(STDOUT_FILENO, (void *)&ch, 1);
+	}
 
 	return 0;
 }
